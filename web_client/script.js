f018ccc88052238cd40a4750b4fd1b2ca7199f1d
Andon1379 <afcb1379@gmail.com>
2021-01-21 23:45:38 -0500

    Co-authored-by: Ryan Davis <waterwolf9910@hotmail.com>

--- web_client/script.js
+++ web_client/script.js
@@ -1,43 +1,44 @@
-const { connect } = require("tls");
-
 var output = document.querySelector("#output"),
-    textarea = document.querySelector("textarea"),
     connected = false,
     connected_to = document.querySelector("div.websocket textarea").value;
 var send = document.getElementById('send_id');
 var output = document.getElementById('out_id');
+var sent_count = 0; 
 
 connected_status.insertAdjacentHTML("afterbegin", "<p id=cnd_stat_2>connected to: " + connected_to + "<br></p>");
 
 function onClickButton_connect() {
     var cnd_2 = document.getElementById('cnd_stat_2');
-    if (connected == true) {
-        onClickButton_disconnect();
-    }
     if (connected == false) {
         send.insertAdjacentHTML("afterbegin", "<textarea cols=60 rows=6 id='send_id2'></textarea><button onclick='onClickButton_send();' id='send_id3'>send</button>");
         connected = true;
         cnd_2.remove();
         connected_to = document.querySelector("div.websocket textarea").value;
         connected_status.insertAdjacentHTML("afterbegin", "<p id=cnd_stat_2>connected to: " + connected_to + "<br></p>");
+    } else {
+        onClickButton_disconnect();
     }
     console.log(connected_to);
 }
 
 function doSend(message) {
     //ws.send(message);
-    console.log('sent: %s', message)
-    writeToScreen("SENT: " + message);
+    console.log('sent: %s', message);
+    writeToScreen_send("SENT: " + message);
 }
 
 function onClickButton_disconnect() {
     var send3 = document.getElementById('send_id3'); 
     var send2 = document.getElementById('send_id2'); // very creative, i know
     var cnd_2 = document.getElementById('cnd_stat_2'); // connected status 2
+    var sent = document.getElementById('sent');
     if (connected == true) {
         send2.remove();
         send3.remove();
         cnd_2.remove();
+        for (i = 0; i < sent_count; i++){
+            sent.remove();
+        }
         connected_status.insertAdjacentHTML("afterbegin", "<p id=cnd_stat_2>connected to: " + connected_to + "<br></p>");
         connected_to =  "";
         connected = false;
@@ -47,14 +48,17 @@ function onClickButton_disconnect() {
 }
 
 function onClickButton_send() {
+    textarea = document.getElementById("send_id2");
     var text = textarea.value;
     text && doSend(text);
+    sent_count++;
+    consle.log(sent_count);
     textarea.value = "";
     textarea.focus();
 }
 
-function writeToScreen(message) {
-    output.insertAdjacentHTML("afterbegin", "<p>" + message + "</p>");
+function writeToScreen_send(message) {
+    output.insertAdjacentHTML("afterbegin", "<p id=sent>" + message + "</p>");
 }
 
 
